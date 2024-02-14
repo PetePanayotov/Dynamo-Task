13f6af65c83ab64dc084e0b330cb18c4a5d55cf4
Peter Panayotov <ppanayotov97@gmail.com>
2024-02-14 14:00:11 +0200

    removed .env file

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,53 +1,54 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 class AxiosClient {
+	private static instance: AxiosClient;
+	private client: AxiosInstance;
 
-    private static instance: AxiosClient;
-    private client: AxiosInstance;
+	private constructor(baseURL: string) {
+		this.client = axios.create({
+			baseURL
+		});
+	}
 
-    private constructor(baseURL: string) {
-        this.client = axios.create({
-            baseURL
-        })
-    };
+	public static getInstance(baseURL: string): AxiosClient {
+		if (!AxiosClient.instance) {
+			AxiosClient.instance = new AxiosClient(baseURL);
+		}
+		return AxiosClient.instance;
+	}
 
-    public static getInstance(baseURL: string): AxiosClient {
-        if (!AxiosClient.instance) {
-            AxiosClient.instance = new AxiosClient(baseURL);
-        }
-        return AxiosClient.instance;
-    }
+	async get(url: string, config?: AxiosRequestConfig) {
+		try {
+			const response = await this.client.get(url, config);
+			return response;
+		} catch (error) {
+			throw new Error(`Request to ${url} failed`);
+		}
+	}
 
-    async get(url: string, config?: AxiosRequestConfig) {
-        try {
-            const response = await this.client.get(url, config);
-            return response;
-        } catch (error) {
-            throw new Error(`Request to ${url} failed`);
-        }
-    };
+	async post(url: string, data?: any, config?: AxiosRequestConfig) {
+		try {
+			const response = await this.client.post(url, data, config);
 
-    async post(url: string, data?: any, config?: AxiosRequestConfig) {
-        try {
-            const response = await this.client.post(url, data, config);
+			return response;
+		} catch (error) {
+			throw new Error(`Request to ${url} failed`);
+		}
+	}
 
-            return response;
-        } catch (error) {
-            throw new Error(`Request to ${url} failed`);
-        }
-    };
+	async put(url: string, data?: any, config?: AxiosRequestConfig) {
+		try {
+			const response = await this.client.put(url, data, config);
 
-    async put(url: string, data?: any, config?: AxiosRequestConfig) {
-        try {
-            const response = await this.client.put(url, data, config);
+			return response;
+		} catch (error) {
+			throw new Error(`Request to ${url} failed`);
+		}
+	}
+}
 
-            return response;
-        } catch (error) {
-            throw new Error(`Request to ${url} failed`);
-        }
-    }
-};
-
-const apiClient = AxiosClient.getInstance(import.meta.env.VITE_API_URL);
+const apiClient = AxiosClient.getInstance(
+	import.meta.env.VITE_API_URL || 'http://localhost:3000'
+);
 
 export default apiClient;
